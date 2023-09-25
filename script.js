103f6154956818b272051dfdc2d86dbc55f9b7a6
Joana Cabral <joanacabralmartins@outlook.com>
2023-09-25 18:44:06 -0300

    contador de visitas

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const btnSalvarLocalmente = document.getElementById('salvarLocalmente')
 btnSalvarLocalmente.addEventListener('click', salvarTextoLocalmente)
 
+atualizarContadorVisitas();
+
 function salvarTextoLocalmente() {
     const variavelSalva = document.getElementById('inputTexto').value
 
@@ -9,4 +11,15 @@ function salvarTextoLocalmente() {
     alert(`Texto "${variavelSalva}" salvo localmente com sucesso!`)
 
     document.getElementById('inputTexto').value = '';
+}
+
+function atualizarContadorVisitas() {
+    if (localStorage.getItem("contadorVisitas") === null) {
+        localStorage.setItem("contadorVisitas", 0)
+    }
+
+    var contador = parseInt(localStorage.getItem("contadorVisitas")) + 1
+    localStorage.setItem("contadorVisitas", contador)
+
+    document.getElementById("visitas").textContent = "Esta página já foi visitada " + contador + " vezes."
 }
